0ef0833fa946b8c84680bbd2ab17e372d4f1cd53
Kay Olan-George <hello@olan-george.com>
2015-11-09 13:33:47 +0000

    Removed some bloatware VS added

--- Kpmg.Assessment/Kpmg.Assessment.WebApi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace Kpmg.Assessment.WebApi.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- Kpmg.Assessment/Kpmg.Assessment.WebApi/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace Kpmg.Assessment.WebApi.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
